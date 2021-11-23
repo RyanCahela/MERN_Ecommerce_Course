8da31f87688c75c2e121f72cdc7675ab04ef145a
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-23 09:34:29 -0800

    HomeScreen layout finished for now

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 function Header() {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" CollapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
           <Navbar.Brand href="/">ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
