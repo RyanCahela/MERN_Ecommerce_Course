c7a8fe952a09f1c835414346a1cbf0bfaccdd388
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-24 10:16:12 -0800

    added morgan and helmet to backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import morgan from "morgan";
+import helmet from "helmet";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -11,14 +13,9 @@ connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} on PORT: ${PORT}`.yellow.bold
-  )
-);
+//middleware
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("api is running...");
@@ -29,3 +26,11 @@ app.use("/api/products", productRoutes);
 //Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on PORT: ${PORT}`.yellow.bold
+  )
+);
