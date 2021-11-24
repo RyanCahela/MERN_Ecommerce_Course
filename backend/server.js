1fe5ba8780409ac7fec5b7b24e6a962b630c92af
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-11-23 16:55:05 -0800

    setup mongoose and connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const products = require("./data/products");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on PORT: ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on PORT: ${PORT}`.yellow.bold
+  )
 );
 
 app.get("/", (req, res) => {
